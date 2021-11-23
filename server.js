c004f4868641b6b2d8600a28b51fbfdfbe3a3a4b
alexdev22 <alejandro2210lp@gmail.com>
2021-11-24 00:14:01 +0100

    Added users array

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 const express =  require('express')
 
+const users = [
+    {
+    name: 'Ander',
+    password: 123456
+    },
+    {
+    name: 'Alphonse',
+    password: 123456
+    },
+    {
+    name: 'Maria',
+    password: 123456
+    },
+    {
+    name: 'Jon',
+    password: 123456
+    },
+    {
+    name: 'Telo',
+    password: 123456
+    },
+    {
+    name: 'Sanjo',
+    password: 123456
+    }
+]
+
+
 const app = express()
 app.get('', (req,res) => {
     res.send('Server')
@@ -8,3 +36,6 @@ app.listen(3000, () => {
     console.log('listening on port 3000');
 })
 
+app.get('/api', (req,res) => {
+    res.json(users)
+})
